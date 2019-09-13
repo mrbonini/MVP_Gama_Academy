f6c6a8c65e63f7d381b2bc2e02fa786b6e29ff42
MrBonini <43351923+mrbonini@users.noreply.github.com>
2019-09-13 04:06:47 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ const input2 = document.getElementById('input2');
 const buttonsContainer = document.getElementById('buttonContainer');
 
 send.addEventListener('click', function(){
-    console.log(input1.value)
+    
     if(input1.value == "PALAVRA-CHAVE" || input2.value == "PALAVRA-CHAVE") {
         alert("insira as palavras chaves para montar o dashboard")
-        console.log(input1.value + " " + input2.value)
+        
     }
     else{
     
@@ -24,10 +24,13 @@ send.addEventListener('click', function(){
         salvo.className = "jsbutton";
         salvo.innerHTML = "SALVO";
 
-    const voltar = document.createElement('button');
+    let voltar = document.createElement('button');
         voltar.className = "jsbutton";
         voltar.innerHTML = "VOLTAR";
         voltar.id = "voltar"
+
+    let retornar = document.createElement('a');
+        retornar.href = "./linte-analytics.html"
     
     let analytics = document.createElement('a');
         analytics.href = "./linte-analytics-dashboard.html"
@@ -36,43 +39,15 @@ send.addEventListener('click', function(){
         analyticsButton.className = "linte";
         analyticsButton.innerHTML = "Linte Analytics Dashboard";
 
-        buttonsContainer.appendChild(salvo);
-        buttonsContainer.appendChild(voltar);
+        buttonsContainer.appendChild(retornar);
+        retornar.appendChild(salvo);
+        buttonsContainer.appendChild(retornar)
+        retornar.appendChild(voltar);
         buttonsContainer.appendChild(analytics);
         analytics.appendChild(analyticsButton);
     }
-
     
-    voltar.addEventListener('click', function() {
-    while (buttonsContainer.hasChildNodes()) {
-        buttonsContainer.removeChild(buttonsContainer.firstChild);
-    }
-    let enviar = document.createElement('button');
-        enviar.className = "envio";
-        enviar.id = "send";
-        enviar.innerHTML = "ENVIAR"
-
-    let cancelar = document.createElement('button');
-        cancelar.className = "cancelar";
-        cancelar.id = "cancel";
-        cancelar.innerHTML = "CANCELAR";
-
-    let analytics = document.createElement('a');
-        analytics.href = "./linte-analytics-dashboard.html"
-
-    let analyticsButton = document.createElement('button');
-        analyticsButton.className = "linte";
-        analyticsButton.innerHTML = "Linte Analytics Dashboard";
-
-        buttonsContainer.appendChild(enviar);
-        buttonsContainer.appendChild(cancelar);
-        buttonsContainer.appendChild(analytics);
-        analytics.appendChild(analyticsButton);
-    
-})
-
 });
-
 
 
 
